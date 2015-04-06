84a37bd7e8821fc0969e4a7d016e1df62380bef4
John Shearer <john@ultranaut.com>
2015-04-06 12:41:32 -0400

    Commenting and formatting problem 1 (java)

--- java/src/problems/Euler.java
+++ java/src/problems/Euler.java
@@ -3,11 +3,9 @@ package problems;
 public class Euler {
 
   public static void main(String[] args) {
-    System.out.println("hello");
 
     ProblemSet problems = new ProblemSet();
-    System.out.println(problems.problem001(3, 5, 1000));
-  }
 
-  
+    System.out.println("Problem 1: " + problems.problem001(3, 5, 1000));
+  }
 }
--- java/src/problems/ProblemSet.java
+++ java/src/problems/ProblemSet.java
@@ -1,26 +1,56 @@
 package problems;
 
 public class ProblemSet {
+  /**
+   *
+   * @param m1
+   * @param m2
+   * @param ceiling
+   * @return
+   */
   public int problem001(int m1, int m2, int ceiling) {
-    /**
+    /*
      * Problem 1: Multiples of 3 and 5
-     * 
-     * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and
-     * 9. The sum of these multiples is 23.
-     * 
+     *
+     * If we list all the natural numbers below 10 that are multiples of
+     * 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+     *
      * Find the sum of all the multiples of 3 or 5 below 1000.
-     * 
+     *
      */
 
-    return arithmeticSum(m1, ceiling) 
+
+    /* The naive method would be to iterate through each integer less
+     * than 1000 and keep a running total of all that are evenly
+     * divisible by either 3 or 5, with O(n) running time.
+     *
+     * A better approach is to compute the answer as the sum of
+     * arithmetic series, i.e. the sum of the multiples of three plus
+     * the sum of the multiples of 5. However, any integers that are
+     * multiples of 15 will have been added twice, so we need to
+     * subtract that sum
+     */
+
+    return arithmeticSum(m1, ceiling)
          + arithmeticSum(m2, ceiling)
-         - arithmeticSum(m1 * m2, ceiling);  
+         - arithmeticSum(m1 * m2, ceiling);
 
   }
 
+  /*
+   * Utility functions
+   */
+
+  /**
+   *
+   * @param step
+   * @param ceiling
+   * @return
+   */
   private int arithmeticSum(int step, int ceiling) {
     int count = (ceiling - 1) / step;
     int largestMultiple = count * step;
     return (largestMultiple + step) * count / 2;
   }
+
 }
