646ffdf5415e83dff434ffc9b6e8ab55d3b4e3e1
John Shearer <john@ultranaut.com>
2015-04-10 22:38:04 -0400

    More stuff

--- java/src/main/Euler.java
+++ java/src/main/Euler.java
@@ -17,43 +17,43 @@ public class Euler {
 
   public static void main(String[] args) {
 
-    // Problem 1
+    /* --- Problem 1 ----------------------------------------------- */
     Problem1 p1 = new Problem1();
     System.out.println(p1);
     System.out.println("\n  Answer: " + p1.solve());
 
     printRule();
 
-    // Problem 2
+    /* --- Problem 2 ----------------------------------------------- */
     Problem2 p2 = new Problem2();
     System.out.println(p2);
     System.out.println("\n  Answer: " + p2.solve());
 
     printRule();
 
-    // Problem 3
+    /* --- Problem 3 ----------------------------------------------- */
     Problem3 p3 = new Problem3();
     System.out.println(p3);
     System.out.println("\n  Answer: " + p3.solve());
 
     printRule();
 
-    // Problem 5
+    /* --- Problem 5 ----------------------------------------------- */
     Problem5 p5 = new Problem5();
     System.out.println(p5);
     System.out.println("\n  Answer: " + p5.solve());
 
     printRule();
 
-    // Problem 6
+    /* --- Problem 6 ----------------------------------------------- */
     Problem6 p6 = new Problem6();
     System.out.println(p6);
     System.out.println("\n  Answer: " + p6.solve());
 
     printRule();
 
-    // Problem 8
-    Problem8 p8 = new Problem8();
+    /* --- Problem 8 ----------------------------------------------- */
+    Problem8 p8 = new Problem8(22);
     System.out.println(p8);
     System.out.println("\n  Answer: " + p8.solve());
 
