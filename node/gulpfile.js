ca7bea5fe9852433cf96ea4991498f3449ffc201
John Shearer <john@ultranaut.com>
2015-05-21 22:38:42 -0400

    Tweak gulpfile

--- node/gulpfile.js
+++ node/gulpfile.js
@@ -3,15 +3,30 @@
 
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
+var plumber = require('gulp-plumber');
+
+function handleError(err) {
+  console.log(err.toString());
+  this.emit('end');
+}
 
 gulp.task('test', function () {
-  return gulp
-    .src('test/*Spec.js')
-    .pipe(mocha());
+  gulp.watch(['./problem_*.js'], function () {
+    return gulp
+      .src('test/*Spec.js')
+      .pipe(mocha())
+      .on('error', handleError);
+  });
+});
+
+gulp.task('temp', function () {
+  gulp.watch(['./problem_029.js', 'test/p29Spec.js'], function () {
+    return gulp
+      .src('test/p29Spec.js')
+      .pipe(mocha({ reporter: 'spec' }))
+      .on('error', handleError);
+  });
 });
 
 gulp.task('default', ['test'], function () {
-  gulp.watch(['./problem_*.js'], function () {
-    gulp.run('test');
-  });
 });
