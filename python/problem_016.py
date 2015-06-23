1d73f3caf6a85b3fc1ea693bff8a4f6c87be9721
John Shearer <john@ultranaut.com>
2015-06-23 01:27:46 -0400

    Problem 16: what if Python didn't support large numbers?

--- python/problem_016.py
+++ python/problem_016.py
@@ -7,7 +7,8 @@ Problem 16: Power digit sum
 What is the sum of the digits of the number 2^1000?
 """
 
-def power_digit_sum(base, exponent):
+# It's trivial in Python...
+def easy_sum(base, exponent):
     num = base**exponent
     _sum = 0
 
@@ -16,4 +17,22 @@ def power_digit_sum(base, exponent):
         num /= 10
 
     return _sum
+
+# ...but let's pretend we don't have support for large numbers
+def multiply(multiplier, base):
+    carry = 0
+    for idx, val in enumerate(base):
+        product = multiplier * val + carry
+        base[idx] = product % 10
+        carry = product / 10
+    if carry != 0:
+        base.append(carry)
+    return base
+
+def power_sum(base, exponent):
+    ints = [base]
+    while exponent > 1:
+        ints = multiply(base, ints)
+        exponent -= 1
+    return sum(ints)
 
