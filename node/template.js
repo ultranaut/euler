202ecb10e01d713854c7312f1b95ed81dde05800
John Shearer <john@ultranaut.com>
2021-01-24 14:07:10 -0500

    Add problem and test templates

---
+++ node/template.js
@@ -0,0 +1,15 @@
+/*eslint-env node*/
+
+/**
+ * Problem title
+ *
+ * Problem description
+ * This is a template
+ * 
+ */
+
+const mySolution = () => {
+  return;
+};
+
+module.exports = mySolution;
---
+++ node/test/template.spec.js
@@ -0,0 +1,8 @@
+/*eslint-env node*/
+const mySolution = require('../template.js');
+
+describe('Problem #: Problem title', () => {
+  it('mySsolution should be a function', () => {
+    expect(typeof mySolution).toBe('function');
+  });
+});
