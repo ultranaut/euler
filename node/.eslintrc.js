e97f5a9de7b5e6e8098da69d36d70c7811292fff
John Shearer <john@ultranaut.com>
2021-06-01 18:43:47 -0400

    Add `jest` to `eslint` config

--- node/.eslintrc.js
+++ node/.eslintrc.js
@@ -2,13 +2,13 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    'jest/globals': true,
   },
   extends: 'eslint:recommended',
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module',
   },
+  plugins: ['jest'],
   rules: {
     'jest/no-disabled-tests': 'warn',
     'jest/no-focused-tests': 'error',
