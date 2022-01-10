131449d0838b9363805d75c0b14d8fb101fe0a35
John Shearer <john@ultranaut.com>
2022-01-10 13:19:24 -0500

    Redo problem 36 (change counting)

--- node/problem_036.js
+++ node/problem_036.js
@@ -12,24 +12,66 @@
  *
  */
 
-const isPalindrome = (str) => {
-  return str === str.split('').reverse().join('');
+const buildPalindrome = (n, odd = false) => {
+  let palindrome = n;
+  if (odd) {
+    n = Math.floor(n / 10);
+  }
+
+  while (n > 0) {
+    palindrome = palindrome * 10 + (n % 10);
+    n = Math.floor(n / 10);
+  }
+  return palindrome;
 };
 
-// could be optimized, this does too much work
+/*
+const isPalindrome = (n, base = 10) => {
+  let reversed = 0;
+  let i = n;
+
+  while (i > 0) {
+    // append last digit to reversed
+    reversed = reversed * base + (i % base);
+    // discard last digit
+    i = Math.floor(i / base);
+  }
+  return n === reversed;
+};
+*/
+
+const isPalindrome = (n) => {
+  let i = 0,
+    j = n.length - 1;
+  while (i <= j) {
+    if (n.charAt(i) !== n.charAt(j)) {
+      return false;
+    }
+    i++, j--;
+  }
+  return true;
+};
+
 const sumPalindromes = (n) => {
+  // number of digits in n
+  let nDigits = Math.floor(Math.log10(n) + 1);
+  // limit on size of number we need to search
+  let maxSize = Math.pow(10, Math.ceil((nDigits + 1) / 2));
+
   let sum = 0;
 
-  // we can skip all the evens because they can't be palindromes in
-  // base 2
-  for (let i = 1; i <= n; i += 2) {
-    const dec = i.toString();
-    const bin = i.toString(2);
-    if (isPalindrome(dec) && isPalindrome(bin)) {
-      sum += i;
+  for (let i = 1; i <= maxSize; i++) {
+    let greater = buildPalindrome(i);
+    let lesser = buildPalindrome(i, true);
+
+    if (greater <= n && isPalindrome(greater.toString(2))) {
+      sum += greater;
+    }
+    if (lesser <= n && isPalindrome(lesser.toString(2))) {
+      sum += lesser;
     }
   }
   return sum;
 };
 
-module.exports = { isPalindrome, sumPalindromes };
+module.exports = { isPalindrome, buildPalindrome, sumPalindromes };
--- node/test/problem_036.spec.js
+++ node/test/problem_036.spec.js
@@ -1,10 +1,28 @@
-const { isPalindrome, sumPalindromes } = require('../problem_036.js');
+/* eslint-env jest */
+const {
+  isPalindrome,
+  buildPalindrome,
+  sumPalindromes,
+} = require('../problem_036.js');
 
 describe('Problem 36: Double-base palindromes', () => {
+  it('should be able to create palindromes', () => {
+    expect(typeof buildPalindrome).toBe('function');
+    expect(buildPalindrome(9, false)).toBe(99);
+    expect(buildPalindrome(9, true)).toBe(9);
+    expect(buildPalindrome(37, false)).toBe(3773);
+    expect(buildPalindrome(37, true)).toBe(373);
+    expect(buildPalindrome(135, false)).toBe(135531);
+    expect(buildPalindrome(135, true)).toBe(13531);
+    expect(buildPalindrome(737, true)).toBe(73737);
+  });
   it('should be able to test palindromes', () => {
     expect(typeof isPalindrome).toBe('function');
-    expect(isPalindrome('abbabba')).toBe(true);
-    expect(isPalindrome('abbabca')).toBe(false);
+    expect(isPalindrome('7337')).toBe(true);
+    expect(isPalindrome('113')).toBe(false);
+    expect(isPalindrome((3).toString(2))).toBe(true);
+    expect(isPalindrome((10).toString(2))).toBe(false);
+    expect(isPalindrome((73737).toString(2))).toBe(true);
   });
   it('should return the sum of palindromes', () => {
     expect(typeof sumPalindromes).toBe('function');
